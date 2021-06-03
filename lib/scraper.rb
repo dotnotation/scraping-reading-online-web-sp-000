8560e198586482aefd4610c010675d676c6b177d
dotnotation <dotinmotionproductions@gmail.com>
2021-06-03 15:15:47 -0400

    commit tree 0471ce0984ea7b1f68b8626687261aff9f32dd69

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
  
-html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
  
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
-courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
+
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC.title-oE5vT4").text.strip
  
 courses.each do |course|
   puts course.text.strip
